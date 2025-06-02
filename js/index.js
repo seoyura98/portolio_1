45a968b4274935fcc00dad74ae5b38ef449e65ae
seoyura98 <dbfk333@naver.com>
2025-06-02 13:13:09 +0900

    포폴

--- js/index.js
+++ js/index.js
@@ -18,6 +18,9 @@ $(function () {
     const content = "예전부터 손으로 사부작사부작 무언가를 만들어 주변사람들에게 선물하는 것을 좋아했습니다. \n\n 저로인해 누군가가 행복함을 느낀다라는 것이 저 자신에게도 행복함과 뿌듯함으로 다가왔습니다. \n\n 웹디자인 또한 화면 속에서 디자인을 직접만들고 사람들에게 만족감을 줄 수 있다라는 점이 비슷하다고 느껴져 자연스럽게 웹디자인에 관심을 갖게 되었습니다. ";
     const text = document.querySelector(".text_box span");
     let i = 0;
+    let start = false;
+    let stop;
+
 
     function typing() {
         let txt = content[i++];
@@ -26,7 +29,7 @@ $(function () {
             clearInterval(stop);
         }
     }
-    stop = setInterval(typing, 100)
+    // stop = setInterval(typing, 100)
 
 
     //con02
@@ -79,6 +82,7 @@ $(function () {
     });
 
 
+
     //con05
     gsap.utils.toArray("section").forEach((section, i) => {
         console.log(i);
@@ -99,8 +103,9 @@ $(function () {
     let con01 = $("#con01").offset().top;
     let con02 = $("#con02").offset().top;
     let con03 = $("#con03").offset().top;
+    let con04 = $("#con04").offset().top;
 
-    console.log(visual, con01);
+    console.log(visual, con01, con02, con03, con04);
 
     $(window).on("scroll", function () {
         let sc = $(this).scrollTop();
@@ -114,14 +119,22 @@ $(function () {
         }
 
         // con01 
-        if (sc >= visual && sc < con01) {
+        if (sc >= visual && sc < con01 && !start) {
             $("#con01 .inner .left").addClass("on");
             $("#con01 .inner .right").addClass("on");
+            start = true;
+            stop = setInterval(typing, 80);
+
         }
 
         //con03
         if (sc >= 2500 && sc < con03) {
             $("#con03 .inner ul").addClass("on");
+        }
+
+        //con04
+        if (sc >= 3500 && sc < con04) {
+            $("#con04 .inner ul").addClass("on");
         }
 
 
